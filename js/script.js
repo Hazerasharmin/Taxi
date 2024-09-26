6ce2a864928136d4aca2ba2b0030bb89cbce6b4f
Hazerasharmin <124484818+Hazerasharmin@users.noreply.github.com>
2024-09-26 22:33:30 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+
+
+//jq
+$(document).ready(function(){
+
+$('.items').slick({
+  slidesToShow: 5,
+  slidesToScroll: 7,
+  autoplay: true,
+  autoplaySpeed: 1500,
+});
+
+
+$('.brands').slick({
+  slidesToShow: 6,
+  slidesToScroll: 7,
+  autoplay: true,
+  autoplaySpeed: 1500,
+});
+
+
+});
+
+
+
+
